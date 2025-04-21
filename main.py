456f9893965110c1b250d20a7bb1acf7eddedda1
Elizamara Choinski <133698710+IzaChoinski@users.noreply.github.com>
2025-04-21 19:33:10 -0300

    alteracao
    alteração do codigo main e readme com melhorias

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ numero = input("Digite um número: ")
 
 
 if numero.isdigit() or (numero.startswith('-') and numero[1:].isdigit()):
-    numero = int(numero)
+    numero = int(numero) 
     if numero % 2 == 0:
-        print(f"O número {numero} é **par**.")
+        print(f"O número {numero} é **par**.") 
     else:
-        print(f"O número {numero} é **ímpar**.")
+        print(f"O número {numero} é **ímpar**.") 
 else:
-    print("Entrada inválida. Por favor, digite um número inteiro.")
+    print("Entrada inválida. Por favor, digite um número inteiro.") 
